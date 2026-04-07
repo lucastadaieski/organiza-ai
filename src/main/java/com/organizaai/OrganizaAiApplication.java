64414a3932300992f62e60ae282e5d0a57c896b3
Lucas Tadaieski <dev.lucastadaieski@gmail.com>
2026-04-06 21:48:26 -0300

    setup initial project structure

---
+++ src/main/java/com/organizaai/OrganizaAiApplication.java
@@ -0,0 +1,13 @@
+package com.organizaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizaAiApplication.class, args);
+	}
+
+}
